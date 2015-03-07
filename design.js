7280b1d63c11d788dea7dfc37e37cd8a80a6cb36
kyleyu714 <kyleyu714@gmail.com>
2015-03-06 20:19:28 -0500

    Update design.js

--- design.js
+++ design.js
@@ -4,31 +4,18 @@ $(document).ready(function(){
 
     $(this).mouseover(function(){
       
-      console.log('mouse over');
-      console.log($(this).find('.image-slide-title').html());
-      
-      
-            $('.image-slide-title').hide();
+      $('.image-slide-title').hide();
             
       $(this).find('.image-slide-title').css('position','absolute');
       $(this).find('.image-slide-title').css('bottom','-50%');
       $(this).find('.image-slide-title').css('font-size','24px');
       $(this).find('.image-slide-title').css('height','100%');
-            $(this).find('.image-slide-title').css('text-align','center');
+      $(this).find('.image-slide-title').css('text-align','center');
       $(this).find('.image-slide-title').css('width','100%');
-            $(this).find('.image-slide-title').css('color','black');
-                        $(this).find('.image-slide-title').css('background','rgba(255, 255, 255, 0.3)');
+      $(this).find('.image-slide-title').css('color','black');
+      $(this).find('.image-slide-title').css('background','rgba(255, 255, 255, 0.3)');
+      $(this).find('.image-slide-title').css('font-family','futura-pt');
 
-            
-
-      
-      $(this).find('.image-slide-title').css('position','absolute');
-      
-      
-      
-      
-      
-      
       $(this).find('.image-slide-title').show();
     });
 
