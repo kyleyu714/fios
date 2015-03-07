84d594ae857b40cb4bf59715eece08020c38cb01
kyleyu714 <kyleyu714@gmail.com>
2015-03-06 19:25:11 -0500

    Update design.js

--- design.js
+++ design.js
@@ -2,11 +2,20 @@ $(document).ready(function(){
 
   $('.sqs-gallery-design-grid-slide').each(function() {      
     console.log($(this).html());
+    
+    $(this).mouseover(function(){
+          $(this).children('.image-slide-title').hide();
+          $(this).children('.image-slide-title').show();
+    });
 
   });
 
 
-  $('.sqs-gallery-design-grid-slide').mouseover(function() {      
+  $('.sqs-gallery-design-grid-slide').mouseover(function() {
+    
+        console.log($(this).html());
+
+    
     $('.sqs-gallery-design-grid-slide').children('.image-slide-title').hide();
     $(this).children('.image-slide-title').show();
   });
