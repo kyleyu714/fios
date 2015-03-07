5756be894fd1a7289bbe66894bdea7eae2f884fc
kyleyu714 <kyleyu714@gmail.com>
2015-03-06 20:00:19 -0500

    Update design.js

--- design.js
+++ design.js
@@ -6,6 +6,25 @@ $(document).ready(function(){
       
       console.log('mouse over');
       console.log($(this).find('.image-slide-title').html());
+      
+      
+            $(this).find('.image-slide-title').hide();
+            
+      $(this).find('.image-slide-title').css('position','absolute');
+      $(this).find('.image-slide-title').css('bottom','-50%');
+      $(this).find('.image-slide-title').css('font-size','24px');
+      $(this).find('.image-slide-title').css('height','100%');
+            $(this).find('.image-slide-title').css('text-align','center');
+      $(this).find('.image-slide-title').css('width','100%');
+
+      
+      $(this).find('.image-slide-title').css('position','absolute');
+      
+      
+      
+      
+      
+      
       $(this).find('.image-slide-title').show();
     });
 
