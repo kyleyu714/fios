ffe62232bcb2f1faea9bf49a54e693cf82379808
kyleyu714 <kyleyu714@gmail.com>
2015-03-06 19:09:35 -0500

    Update design.js

--- design.js
+++ design.js
@@ -1,12 +1,9 @@
 $(document).ready(function(){
 
-  alert('Welcome');  
-  
-  /*
+
   $('.design-grid-slide').mouseover(function() {      
     $('.design-grid-slide').children('.hiddenx').hide();
       $(this).children('.hiddenx').show();
     });
-  */
-  
+
 });
